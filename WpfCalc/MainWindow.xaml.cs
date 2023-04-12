49e78e6cbed3f2a15c6ace9792d83c4be2fb5414
Sergey <94394415+des-808@users.noreply.github.com>
2023-04-13 02:34:16 +0300

    xz

--- WpfCalc/MainWindow.xaml.cs
+++ WpfCalc/MainWindow.xaml.cs
@@ -28,59 +28,63 @@ namespace WpfCalc
         CalculatoR calculatoR = new CalculatoR();
         private string? xxx;
         bool bool_deystvie = false;
+        double tmp;
+        double _OtveT_;
+        double _Left_;
+        double _Right_;
         private void btn_CE_click(object sender, RoutedEventArgs e){ new_sample.Text = "0"; old_sample.Text = ""; }// сбросить всё
         private void btn_C_click(object sender, RoutedEventArgs e) { new_sample.Text = "0"; }//сбросить последнее выражение
         private void btn_Del_click(object sender, RoutedEventArgs e){ 
             if (new_sample.Text.Length> 0) { new_sample.Text = new_sample.Text.Remove(new_sample.Text.Length - 1); }// вырезаем последний символ
         }
-        
+
         //private void btn_1_click(object sender, RoutedEventArgs e)      { del_new_samle(); xxx = new_sample.Text.Equals("0") ? new_sample.Text = "1" : new_sample.Text += "1"; }
-        private void btn_1_click(object sender, RoutedEventArgs e)      { del_new_sample(); xxx = iss_zero(new_sample.Text, "1"); }
-        private void btn_2_click(object sender, RoutedEventArgs e)      { del_new_sample(); xxx = iss_zero(new_sample.Text, "2"); }
-        private void btn_3_click(object sender, RoutedEventArgs e)      { del_new_sample(); xxx = iss_zero(new_sample.Text, "3"); }
-        private void btn_4_click(object sender, RoutedEventArgs e)      { del_new_sample(); xxx = iss_zero(new_sample.Text, "4"); }
-        private void btn_5_click(object sender, RoutedEventArgs e)      { del_new_sample(); xxx = iss_zero(new_sample.Text, "5"); }
-        private void btn_6_click(object sender, RoutedEventArgs e)      { del_new_sample(); xxx = iss_zero(new_sample.Text, "6"); }
-        private void btn_7_click(object sender, RoutedEventArgs e)      { del_new_sample(); xxx = iss_zero(new_sample.Text, "7"); }
-        private void btn_8_click(object sender, RoutedEventArgs e)      { del_new_sample(); xxx = iss_zero(new_sample.Text, "8"); }
-        private void btn_9_click(object sender, RoutedEventArgs e)      { del_new_sample(); xxx = iss_zero(new_sample.Text, "9"); }
-        private void btn_0_click(object sender, RoutedEventArgs e)      { del_new_sample(); xxx = iss_zero(new_sample.Text, "0"); }
+        private void btn_1_click(object sender, RoutedEventArgs e)      { del_new_sample(); _Right_ = Convert.ToDouble(iss_zero(new_sample.Text, "1")); }//xxx = iss_zero(new_sample.Text, "1"); _Right_ = Convert.ToDouble(xxx); }
+        private void btn_2_click(object sender, RoutedEventArgs e)      { del_new_sample(); _Right_ = Convert.ToDouble(iss_zero(new_sample.Text, "2")); }//xxx = iss_zero(new_sample.Text, "2"); _Right_ = Convert.ToDouble(xxx); }
+        private void btn_3_click(object sender, RoutedEventArgs e)      { del_new_sample(); _Right_ = Convert.ToDouble(iss_zero(new_sample.Text, "3")); }//xxx = iss_zero(new_sample.Text, "3"); _Right_ = Convert.ToDouble(xxx); }
+        private void btn_4_click(object sender, RoutedEventArgs e)      { del_new_sample(); _Right_ = Convert.ToDouble(iss_zero(new_sample.Text, "4")); }//xxx = iss_zero(new_sample.Text, "4"); _Right_ = Convert.ToDouble(xxx); }
+        private void btn_5_click(object sender, RoutedEventArgs e)      { del_new_sample(); _Right_ = Convert.ToDouble(iss_zero(new_sample.Text, "5")); }//xxx = iss_zero(new_sample.Text, "5"); _Right_ = Convert.ToDouble(xxx); }
+        private void btn_6_click(object sender, RoutedEventArgs e)      { del_new_sample(); _Right_ = Convert.ToDouble(iss_zero(new_sample.Text, "6")); }//xxx = iss_zero(new_sample.Text, "6"); _Right_ = Convert.ToDouble(xxx); }
+        private void btn_7_click(object sender, RoutedEventArgs e)      { del_new_sample(); _Right_ = Convert.ToDouble(iss_zero(new_sample.Text, "7")); }//xxx = iss_zero(new_sample.Text, "7"); _Right_ = Convert.ToDouble(xxx); }
+        private void btn_8_click(object sender, RoutedEventArgs e)      { del_new_sample(); _Right_ = Convert.ToDouble(iss_zero(new_sample.Text, "8")); }//xxx = iss_zero(new_sample.Text, "8"); _Right_ = Convert.ToDouble(xxx); }
+        private void btn_9_click(object sender, RoutedEventArgs e)      { del_new_sample(); _Right_ = Convert.ToDouble(iss_zero(new_sample.Text, "9")); }//xxx = iss_zero(new_sample.Text, "9"); _Right_ = Convert.ToDouble(xxx); }
+        private void btn_0_click(object sender, RoutedEventArgs e)      { del_new_sample(); _Right_ = Convert.ToDouble(iss_zero(new_sample.Text, "0")); }//xxx = iss_zero(new_sample.Text, "0"); _Right_ = Convert.ToDouble(xxx); }
         private void btn_Tchk_click(object sender, RoutedEventArgs e)   { new_sample.Text += "."; }
-        private void btn_Add_click(object sender, RoutedEventArgs e)    { old_sample.Text = "+"; old_sample.Text += new_sample.Text; bool_deystvie = true; operaciya = '+'; deystviee(); }
-        private void btn_Sub_click(object sender, RoutedEventArgs e)    { old_sample.Text = "-"; old_sample.Text += new_sample.Text; bool_deystvie = true; operaciya = '-'; deystviee(); }
-        private void btn_Mul_click(object sender, RoutedEventArgs e)    { old_sample.Text = "*"; old_sample.Text += new_sample.Text; bool_deystvie = true; operaciya = '*'; deystviee(); }
-        private void btn_Div_click(object sender, RoutedEventArgs e)    { if (!new_sample.Text.Equals("0")) { old_sample.Text = "/"; old_sample.Text += new_sample.Text; bool_deystvie = true; operaciya = '/'; deystviee(); } }
+        private void btn_Add_click(object sender, RoutedEventArgs e)    { old_sample.Text = "+"; old_sample.Text += new_sample.Text; bool_deystvie = true; operaciya = '+'; }
+        private void btn_Sub_click(object sender, RoutedEventArgs e)    { old_sample.Text = "-"; old_sample.Text += new_sample.Text; bool_deystvie = true; operaciya = '-'; }
+        private void btn_Mul_click(object sender, RoutedEventArgs e)    { old_sample.Text = "*"; old_sample.Text += new_sample.Text; bool_deystvie = true; operaciya = '*'; }
+        private void btn_Div_click(object sender, RoutedEventArgs e)    { if (!new_sample.Text.Equals("0")) { old_sample.Text = "/"; old_sample.Text += new_sample.Text; bool_deystvie = true; operaciya = '/'; } }
         private void btn_Equals_click(object sender, RoutedEventArgs e){
-            string old_text = old_sample.Text;
+    string revers_str = calculatoR.revers_string( old_sample.Text);
             char[] delimiterChars = { '+', '-', '*', '/','='};
-            //char[] delimiterChars = { '='};
-            char[] delimiterInt = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0','=' };
-            string[] xz = old_sample.Text.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
+            char[] delimiterInt = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+            //string[] xz = old_sample.Text.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
+            string[] xz = revers_str.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
             string[] xy = new_sample.Text.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
-            string[] ot = old_text.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
-            double Left = Convert.ToDouble(xz[0]);
-            //double Right = Convert.ToDouble(xy[0]);
+            _Right_ = _Right_;
+            //if(xz.Length==1)
             double Right = Convert.ToDouble(xy[0]);
+
+            if (xz.Length>1) { double Left = xz.Length > 0 ? Convert.ToDouble(xz[1]) : Convert.ToDouble(xz[0]); } 
+            else {
+                MessageBox.Show(xxx);
+            }
             double otvet = calculatoR.Operation(Left, Right, operaciya);
-            
-            string otvet_string = otvet.ToString();
+            //string otvet_string = calculatoR.revers_string(otvet.ToString());
+            string otvet_string =otvet.ToString();
             if (otvet < 0) { otvet_string = calculatoR.revers_string(otvet_string); }
-            //old_sample.Text = $"{Left}{operators}{Right}={otvet}";
-            //old_sample.Text = ot[0] +operaciya+xxx+'=';
-            old_sample.Text = $"={calculatoR.revers_string(old_sample.Text)+new_sample.Text}";//$"{Left}{operators}{Right}={otvet}";
-            //string wwww = ot[0] + operaciya + xxx + '=';
-            //old_sample.Text = revers_string( wwww);
-            //old_sample.Text = $"={revers_string(ot[0]) +new_sample.Text}";//$"{Left}{operators}{Right}={otvet}";
             new_sample.Text = otvet_string;
+            old_sample.Text = $"={Left}{operaciya}{Right}";
+            //old_sample.Text = $"={Right}{operaciya}{Left}";
+            //old_sample.Text = $"={calculatoR.revers_string(old_sample.Text)+new_sample.Text}";//$"{Left}{operators}{Right}={otvet}";
+
         }
         private string iss_zero(string text,string btn_number) 
         {
              text = text.Equals("0") ? new_sample.Text = btn_number : new_sample.Text += btn_number;
             return text; 
         }
-        private void deystviee() {
-            
-        }
+       
         private void del_new_sample() { if (bool_deystvie) { new_sample.Text = ""; bool_deystvie = false; } }
         
         class CalculatoR {
@@ -90,8 +94,11 @@ namespace WpfCalc
                 char[] delimiterInt = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
                 string[] zx = text.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
                 string[] zy = text.Split(delimiterInt, StringSplitOptions.RemoveEmptyEntries);
-                if (zx.Length > 1 && zy.Length > 1) { return zx[0] + zy[0] + zx[1] + zy[1]; }
-                return zx[0] + zy[0];
+                string tmp = "";
+                for(int i = 0,j = 0; i < zx.Length; i++, j++) {
+                tmp += zx[i]; if (j < zy.Length) { tmp += zy[j]; }
+                }
+                return tmp;
             }
             public double Operation(double Left, double Right, char deystvie) {
                 double otvet = 0;
